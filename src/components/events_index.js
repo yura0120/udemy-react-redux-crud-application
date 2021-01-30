8ae251d5e4ee49c028e7dc7fc434dd0e653e8fb1
yura0120 <hyuri120.as@gmail.com>
2021-01-30 15:48:14 +0900

    implement event new component

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import _ from 'lodash'
+import { Link } from 'react-router-dom'
 
 import { readEvents } from '../actions'
 
@@ -21,6 +22,7 @@ class EventsIndex extends Component{
 
   render() {
     return (
+      <React.Fragment>
       <table>
         <thead>
           <tr>
@@ -34,6 +36,9 @@ class EventsIndex extends Component{
           {this.renderEvents()}
         </tbody>
       </table>
+
+      <Link to="/events/new">New Event</Link>
+      </React.Fragment>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './index.css';
 import reducer from './reducers'
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 import reportWebVitals from './reportWebVitals';
 
 const store = createStore(reducer, applyMiddleware(thunk))
@@ -14,7 +16,12 @@ const store = createStore(reducer, applyMiddleware(thunk))
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <EventsIndex />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/events/new' component={EventsNew} />
+          <Route exact path='/' component={EventsIndex} />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
